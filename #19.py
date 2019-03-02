a230d8d38ff33ba6fe9a74c2c02dd77e218560e6
stelioszlat <steliosdj6@gmail.com>
2019-03-02 13:03:42 +0200

    solved #20

--- #19.py
+++ #19.py
@@ -1,3 +1,34 @@
 import calendar as c
 
+months = {'January':31, 'February':28, 'March':31,'April':30,'May':31,
+        'June':30, 'July':31, 'August':31, 'September':30, 'October':31,
+            'November':30, 'December':31}
 
+lmonths = {'January':31, 'February':29, 'March':31,'April':30,'May':31,
+        'June':30, 'July':31, 'August':31, 'September':30, 'October':31,
+            'November':30, 'December':31}
+
+week = {1:'Monday', 2:'Tuesday', 3:'Wedsesday', 4:'Thursday',
+            5:'Friday', 6:'Saturday', 7:'Sunday'}
+
+sundays = 0
+year = 1900
+days = 0
+last = 0
+
+for i in range(1900, 2001):
+
+    if c.isleap(i):
+
+        for value in lmonths.values():
+            for j in range(value):
+                days += 1
+
+    else:
+
+        for value in months.values():
+            for j in range(value):
+                days += 1
+
+
+print(sundays)
--- Tools/factors.py
+++ Tools/factors.py
@@ -7,9 +7,15 @@ def factors(n):
             factors.append(i)
     return factors
 
-
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 if __name__ == '__main__':
     nums = [1, 3, 6, 10, 15, 21, 28]
     for i in nums:
         print(factors(i))
+
+    # print(factorial(15))
