eca9b03cf96009303da06dbf40d6fbcb848ffe3a
Zlat <steliosdj6@gmail.com>
2019-10-25 22:03:34 +0300

    solved #29

--- #29.py
+++ #29.py
@@ -1,10 +1,12 @@
 
 combinations = []
 
-for i in range(2, 100):
-    for j in range(2, 100):
-        combinations.append(i**2)
+for i in range(2, 101):
+    for j in range(2, 101):
+        combinations.append(i**j)
 
 c = set(combinations)
 
-print(c)
+print(len(c))
+
+# solution 9183
