228d8f3cfa7f50e8a3753504119c9e790e56464a
stelioszlat <steliosdj6@gmail.com>
2021-02-14 16:01:20 +0200

    changed return statement to yield in factors()

--- Tools/factors.py
+++ Tools/factors.py
@@ -4,8 +4,7 @@ def factors(n):
     factors = []
     for i in range(1, n+1):
         if n % i == 0:
-            factors.append(i)
-    return factors
+            yield factors
 
 def factorial(n):
     if n == 0 or n == 1:
